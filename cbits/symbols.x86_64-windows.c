a53c57c9a8d22a66a2f0c4c969e806da03f08c28
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2025-06-22 10:01:13 +1200

    Fix GHC 9.6 mingwW64 cross compile

--- cbits/symbols.x86_64-windows.c
+++ cbits/symbols.x86_64-windows.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <math.h>
 
 #define MISSING_FUN(f) void (f)(void) { printf("Unknown call to `%s'\n", #f); exit(1); }
@@ -30,7 +31,49 @@ typedef struct _RtsSymbolVal {
 #define SYM(x) { #x, (void*)(&x), STRENGTH_NORMAL, 1 }
 
 RtsSymbolVal my_iserv_syms[] = {
-    SYM(log1pf), SYM(log1p), SYM(logf), SYM(log),
+    SYM(acos),
+    SYM(acosf),
+    SYM(acosh),
+    SYM(acoshf),
+    SYM(asin),
+    SYM(asinf),
+    SYM(asinh),
+    SYM(asinhf),
+    SYM(atan),
+    SYM(atanf),
+    SYM(atanh),
+    SYM(atanhf),
+    SYM(cos),
+    SYM(cosf),
+    SYM(cosh),
+    SYM(coshf),
+    SYM(erf),
+    SYM(erfc),
+    SYM(erfcf),
+    SYM(erff),
+    SYM(exp),
+    SYM(expf),
+    SYM(expm1),
+    SYM(expm1f),
+    SYM(log),
+    SYM(log1p),
+    SYM(log1pf),
+    SYM(logf),
+    SYM(memcmp),
+    SYM(memcpy),
+    SYM(memmove),
+    SYM(memset),
+    SYM(pow),
+    SYM(powf),
+    SYM(sin),
+    SYM(sinf),
+    SYM(sinh),
+    SYM(sinhf),
+    SYM(strlen),
+    SYM(tan),
+    SYM(tanf),
+    SYM(tanh),
+    SYM(tanhf),
     { 0, 0, STRENGTH_NORMAL, 1 } /* sentinel */
 };
 
